b5f6bc5aacf5788b3da9c45753318fd6974c9766
Manuel Odelain <manuel@rezo-zero.com>
2024-02-22 17:11:15 +0100

    chore: install playground with latest Nuxt

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,15 +1,4 @@
-import { fileURLToPath } from 'url'
-import { dirname, join } from 'path'
-
-// @see https://nuxt.com/docs/guide/going-further/layers#relative-paths-and-aliases
-const currentDir = dirname(fileURLToPath(import.meta.url))
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: { enabled: true },
-    extends: ['../'],
-    alias: {
-        '~layer': join(currentDir, '../'),
-    },
-    watch: [join(currentDir, '../')],
+  extends: ['../']
 })
