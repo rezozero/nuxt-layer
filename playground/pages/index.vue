0d99dbadecddb2f47c573772342560aadf15b24c
Manuel Odelain <manuel@rezo-zero.com>
2024-02-27 11:19:49 +0100

    feat: add Nuxt image and intervention request provider

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -2,7 +2,8 @@
 
 <template>
     <div>
-        <NuxtWelcome />
+        <h1>Home page</h1>
+        <NuxtImg src="rhino.jpg" sizes="xs:100vw lg:100vw" alt="Rhino test" style="max-width: 300px" />
     </div>
 </template>
 
