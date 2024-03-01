947685bbe6bed453aeee4a348df1d49c1f26b320
Manuel Odelain <manuel@rezo-zero.com>
2024-03-01 17:33:52 +0100

    refactor: all the components on the root components/ directory

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,4 @@
-import { fileURLToPath } from 'url'
-import { dirname, join } from 'path'
-
-// @see https://nuxt.com/docs/guide/going-further/layers#relative-paths-and-aliases
-const currentDir = dirname(fileURLToPath(import.meta.url))
-
 export default defineNuxtConfig({
-    components: [
-        join(currentDir, './components/atoms'),
-        join(currentDir, './components/molecules'),
-        join(currentDir, './components/organisms'),
-    ],
     runtimeConfig: {
         public: {
             site: {
