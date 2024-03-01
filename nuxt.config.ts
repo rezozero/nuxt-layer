20c5d8ab01f466253abc11305e365ca69b2e549f
Manuel Odelain <manuel@rezo-zero.com>
2024-03-01 14:11:00 +0100

    chore: remove nuxt modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,38 +5,9 @@ import { dirname, join } from 'path'
 const currentDir = dirname(fileURLToPath(import.meta.url))
 
 export default defineNuxtConfig({
-    devtools: { enabled: true },
-    // css: [join(currentDir, './assets/scss/main.scss')],
-    modules: ['@rezo-zero/intervention-request-provider', '@nuxt/image', '@rezo-zero/nuxt-stories'],
     components: [
         join(currentDir, './components/atoms'),
         join(currentDir, './components/molecules'),
         join(currentDir, './components/organisms'),
     ],
-    runtimeConfig: {
-        public: {
-            site: {
-                url: 'http://localhost:3000',
-            },
-        },
-    },
-    // Nuxt image
-    // https://image.nuxt.com/get-started/configuration
-    image: {
-        provider: 'interventionRequest',
-        quality: 75,
-        screens: {
-            xs: 375, // override size to match our breakpoints
-            xl: 1440, // override size to match our breakpoints
-            hd: 1920, // additional size
-            qhd: 2500, // additional size
-        },
-        // @ts-ignore not working with [1]
-        densities: '1',
-        presets: {
-            default: {
-                sizes: 'xs:100vw md:100vw lg:100vw vl:100vw xl:100vw hd:100vw qhd:100vw',
-            },
-        },
-    },
 })
--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -3,7 +3,6 @@
 <template>
     <div>
         <h1>Home page</h1>
-        <NuxtImg src="rhino.jpg" sizes="xs:100vw lg:100vw" alt="Rhino test" style="max-width: 300px" />
     </div>
 </template>
 
