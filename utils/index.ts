b662643ad74268c4b5eec6bc53fee8a570af40a4
Manuel Odelain <manuel@rezo-zero.com>
2024-03-15 14:51:43 +0100

    chore(utils): use wildcard for all imports

--- utils/index.ts
+++ utils/index.ts
@@ -2,9 +2,9 @@
 // @see https://nuxt.com/docs/guide/directory-structure/composables#how-files-are-scanned
 
 // string
-export { b64DecodeUnicode } from './string/b64-decode-unicode'
+export * from './string/b64-decode-unicode'
 // Roadiz
-export { getResponseAlternateLinks } from './roadiz/get-response-alternate-links'
+export * from './roadiz/get-response-alternate-links'
 export * from './roadiz/document'
 export * from './roadiz/entity'
 export * from './roadiz/node-sources'
