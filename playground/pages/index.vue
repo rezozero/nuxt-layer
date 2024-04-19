6d91b9474b46359b71d4342c92eb380af9076bf0
Ambroise Maupate <ambroise@rezo-zero.com>
2024-04-19 10:51:36 +0200

    chore: Removed cache-control.ts (moved to a nuxt module)

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -3,19 +3,6 @@ import { getStructuredDataScript } from '../../utils/structured-data'
 
 const runtimeConfig = useRuntimeConfig()
 
-/*
- * Define SSR cache control header for this page
- */
-useCacheControl({
-    public: true,
-    // 1 hour
-    maxAge: 60 * 60,
-    // 1 hour
-    sMaxAge: 60 * 60,
-    // SWR for 2 minutes
-    staleWhileRevalidate: 60 * 2,
-})
-
 useHead({
     script: [
         getStructuredDataScript({
