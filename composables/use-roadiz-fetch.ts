3155b45735a18905a778ec33ad677fb3a260a839
Manuel Odelain <manuel@rezo-zero.com>
2024-06-06 12:33:13 +0200

    feat(roadiz fetch): handle auto key

--- composables/use-roadiz-fetch.ts
+++ composables/use-roadiz-fetch.ts
@@ -21,9 +21,34 @@ export function useRoadizFetch<
 >(
     request: Ref<ReqT> | ReqT | (() => ReqT),
     opts?: UseFetchOptions<_ResT, DataT, PickKeys, DefaultT, ReqT, Method>,
-): AsyncData<PickFrom<DataT, PickKeys> | DefaultT, ErrorT | null> {
-    return useFetch<ResT, ErrorT, ReqT, Method, _ResT, DataT, PickKeys, DefaultT>(request, {
-        ...opts,
-        $fetch: opts?.$fetch || useRoadizFetchFactory(),
-    })
+): AsyncData<PickFrom<DataT, PickKeys> | DefaultT, ErrorT | null>
+export function useRoadizFetch<
+    ResT = void,
+    ErrorT = FetchError,
+    ReqT extends NitroFetchRequest = NitroFetchRequest,
+    Method extends AvailableRouterMethod<ReqT> = ResT extends void
+        ? 'get' extends AvailableRouterMethod<ReqT>
+            ? 'get'
+            : AvailableRouterMethod<ReqT>
+        : AvailableRouterMethod<ReqT>,
+    _ResT = ResT extends void ? FetchResult<ReqT, Method> : ResT,
+    DataT = _ResT,
+    PickKeys extends KeysOf<DataT> = KeysOf<DataT>,
+    DefaultT = DataT,
+>(
+    request: Ref<ReqT> | ReqT | (() => ReqT),
+    arg1?: string | Omit<UseFetchOptions<_ResT, DataT, PickKeys, DefaultT, ReqT, Method>, 'lazy'>,
+    arg2?: string,
+) {
+    const [opts = {}, autoKey] = typeof arg1 === 'string' ? [{}, arg1] : [arg1, arg2]
+
+    return useFetch<ResT, ErrorT, ReqT, Method, _ResT, DataT, PickKeys, DefaultT>(
+        request,
+        {
+            ...opts,
+            $fetch: opts?.$fetch || useRoadizFetchFactory(),
+        },
+        // @ts-expect-error we pass an extra argument with the resolved auto-key to prevent another from being injected
+        autoKey,
+    )
 }
